c74124824eb897f8cb3d09758073744560c7d73b
The Linux Choice <34893261+thelinuxchoice@users.noreply.github.com>
2020-03-13 17:52:12 -0400

    Update underhanded.sh

--- underhanded.sh
+++ underhanded.sh
@@ -46,7 +46,6 @@ exit 1
 dependencies() {
 
 
-command -v httrack > /dev/null 2>&1 || { echo >&2 "I require Httrack but it's not installed. Install it. Aborting."; exit 1; }
 command -v php > /dev/null 2>&1 || { echo >&2 "I require php but it's not installed. Install it. Aborting."; exit 1; }
 
 
